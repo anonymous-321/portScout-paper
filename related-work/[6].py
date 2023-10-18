06c057e184279ac78ba72aee3140867fc5a5a163
Khattak01 <mskhattak.ncs@gmail.com>
2023-10-18 11:36:41 +0500

    database time duration info added

--- related-work/[6].py
+++ related-work/[6].py
@@ -1,65 +1,144 @@
 from scapy.all import *
 import math
+from scapy.all import PcapReader
+from collections import defaultdict
 
-from scapy.all import *
+packets = []
 
-def extract_packet_data(raw_packet):
+def extract_packet_data(packet):
     try:
-        packet = Ether(raw_packet)  # Assuming it's an Ethernet packet, adjust as needed
 
-        src_ip = packet[IP].src
-        dst_ip = packet[IP].dst
-        timestamp = str(packet.time)
+        src_ip = dst_ip = src_port = dst_port = timestamp = ""
+
+        # packet.show()
+
+        if IP in packet:
+            src_ip = packet[IP].src
+            dst_ip = packet[IP].dst
 
         if TCP in packet:
             src_port = packet[TCP].sport
             dst_port = packet[TCP].dport
-            # protocol = "TCP"
         elif UDP in packet:
             src_port = packet[UDP].sport
             dst_port = packet[UDP].dport
-            # protocol = "TCP"
         elif SCTP in packet:
             src_port = packet[SCTP].sport
             dst_port = packet[SCTP].dport
-            # protocol = "SCTP"
-        else:
-            # Handle other protocols as needed
-            src_port = 0
-            dst_port = 0
-            # protocol = "Unknown"
+
+        timestamp = str(packet.time)
 
         return {
             "src_ip": src_ip,
             "dst_ip": dst_ip,
             "src_port": src_port,
             "dst_port": dst_port,
-            # "protocol": protocol,
             "timestamp": timestamp,
         }
     except Exception as e:
         print(f"Error extracting packet data: {e}")
-        return {}
+
+
+# def calculate_spd_per_ip(packets):
+#     # Create dictionaries to keep track of source and destination flows for each IP
+#     source_flows = defaultdict(int)
+#     destination_flows = defaultdict(int)
+#     spd_per_ip = {}
+
+#     for packet in packets:
+#         src_ip = packet["src_ip"]
+#         dst_ip = packet["dst_ip"]
+
+#         # Count source and destination flows for each IP
+#         source_flows[src_ip] += 1
+#         destination_flows[dst_ip] += 1
+
+#     for ip in source_flows.keys():
+#         spd = math.log(source_flows[ip] / destination_flows[ip])
+#         spd_per_ip[ip] = spd
+
+#     return spd_per_ip
+
+# Define a function to calculate SPD (Source per Destination)
+def calculate_spd_for_ip(packets, ip):
+    source_flows = 0
+    destination_flows = 0
+
+    for packet in packets:
+        src_ip = packet["src_ip"]
+        dst_ip = packet["dst_ip"]
+
+        if src_ip == ip:
+            source_flows += 1
+
+        if dst_ip == ip:
+            destination_flows += 1
+
+    if destination_flows == 0:
+        return float("inf")  # Handle the case where the denominator is zero
+
+    spd = math.log(source_flows / destination_flows)
+    return spd
+
 
 
 # Define a function to calculate PPI (Ports per IPs)
-def calculate_ppi(src_ports, dst_ip):
-    try:
-        ppi = math.log(src_ports / dst_ip)
-        return ppi
-    except Exception as e:
-        print(f"Error calculating PPI: {e}")
-        return 0
+# def calculate_ppi(packets):
+#     destination_ports = defaultdict(int)
+#     destination_ip_addresses = defaultdict(int)
+#     ppi_per_ip = {}
 
-# Define a function to calculate SPD (Source per Destination)
-def calculate_spd(src_ip, dst_ip):
-    try:
-        spd = math.log(src_ip / dst_ip)
-        return spd
-    except Exception as e:
-        print(f"Error calculating SPD: {e}")
-        return 0
+#     for packet in packets:
+#         dst_ip = packet["dst_ip"]
+#         dst_port = packet["dst_port"]
 
-def main(pkt):
-    packet = extract_packet_data(pkt)
+#         destination_ports[dst_ip] += 1
+#         destination_ip_addresses[dst_ip] += 1
+
+#     for ip in destination_ports.keys():
+#         if destination_ip_addresses[ip] == 0:
+#             ppi = float("inf")  # Handle the case where the denominator is zero
+#         else:
+#             ppi = math.log(destination_ports[ip] / destination_ip_addresses[ip])
+#         ppi_per_ip[ip] = ppi
+
+#     return ppi_per_ip
+
+
+def calculate_ppi_for_ip(packets, ip):
+    destination_ports = 0
+    destination_ip_addresses = 0
+
+    for packet in packets:
+        dst_ip = packet["dst_ip"]
+        dst_port = packet["dst_port"]
+
+        if dst_ip == ip:
+            destination_ports += 1
+            destination_ip_addresses += 1
+
+    if destination_ip_addresses == 0:
+        ppi = float("inf")  # Handle the case where the denominator is zero
+    else:
+        ppi = math.log(destination_ports / destination_ip_addresses)
+
+    return ppi
+
+
+if __name__=="__main__":
+
+    # Replace 'pcap_file' with the path to your pcap file
+    pcap_file = '//home/khattak01/Desktop/thesis/tests/packets-500.pcap'
+
+
+    # Open the pcap file using PcapReader
+    with PcapReader(pcap_file) as pcap_reader:
+        # Loop through packets
+        for packet in pcap_reader:
+
+            packet = extract_packet_data(packet)
+            packets.append(packet)
+
+            print(packet)
+            print(len(packets))
 
